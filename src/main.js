d0609fd3f0bfc4b52cdf5bc47ac0d55d644a2eac
Timurbek <timurbekbeksaburov.com>
2024-09-04 16:19:33 +0500

    backend comp

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import { appConfig } from './config/app.config.js';
+import { router } from './routes/index.js';
 const app = express()
 
 //MIDDLEWARES
 app.use(morgan('tiny'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use('/api/v1',router)
 
 
 app.listen(appConfig.port, appConfig.host, () => {
